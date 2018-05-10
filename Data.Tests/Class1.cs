91ca52029c15782ea401c8f427cdca3fefb58159
Arlo Belshee <arlo.belshee@live.com>
2018-05-10 02:55:56 +0200

    a     R# code cleanup to new settings.

--- Data.Tests/Class1.cs
+++ Data.Tests/Class1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Data.Tests
+﻿namespace Data.Tests
 {
-    public class Class1
-    {
-    }
+	public class Class1
+	{
+	}
 }
