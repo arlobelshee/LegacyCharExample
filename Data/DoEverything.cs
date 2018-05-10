6758ed92f66081f9d45629c9639294d9361b2910
Arlo Belshee <arlo.belshee@live.com>
2018-05-10 03:03:33 +0200

    F     define the API.

--- Data/DoEverything.cs
+++ Data/DoEverything.cs
@@ -1,6 +1,12 @@
-﻿namespace Data
+﻿using JetBrains.Annotations;
+
+namespace Data
 {
 	public class DoEverything
 	{
+		public CharacterData MakeAllTheViewModels([NotNull] string fileName)
+		{
+			return new CharacterData();
+		}
 	}
 }
