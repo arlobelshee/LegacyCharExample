61d5ca740aab5457bfb0fdc367a59015976e1094
Arlo Belshee <arlo.belshee@live.com>
2018-05-10 03:27:48 +0200

    F     write out the template method and all the things it uses. Pretend this was many commits, with real code, that built up these features incrementally.

---
+++ Data/CompendiumService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Data
+{
+	public class CompendiumService
+	{
+		[NotNull]
+		public static CompendiumService Authenticate([NotNull] string username, [NotNull] string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void FillOutFlavorText(CardData card)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Data/DoEverything.cs
+++ Data/DoEverything.cs
@@ -1,12 +1,37 @@
-﻿using JetBrains.Annotations;
+﻿using System.Linq;
+using JetBrains.Annotations;
 
 namespace Data
 {
 	public class DoEverything
 	{
-		public CharacterData MakeAllTheViewModels([NotNull] string fileName)
+		[NotNull]
+		public CharacterData MakeAllTheViewModels([NotNull] string fileName, [NotNull] string username,
+			[NotNull] string password)
 		{
-			return new CharacterData();
+			var characterFile = CharacterFile.From(fileName);
+			var configFile = ConfigFile.Matching(characterFile);
+			var partialCards = characterFile.ParseCards();
+			var localCards = configFile.ParseCards();
+			var compendiumService = CompendiumService.Authenticate(username, password);
+			var cardService = CardService.Authenticate(username, password);
+			foreach (var card in partialCards)
+			{
+				cardService.FetchDetailsInto(card);
+				compendiumService.FillOutFlavorText(card);
+				_LocateAndTranslateFormulas(card);
+				cardService.ResolveReferencesToOtherCards(card);
+			}
+			foreach (var card in localCards.Concat(partialCards))
+			{
+				characterFile.ResolveFormulasToValues(card, configFile);
+			}
+			return new CharacterData(localCards.Concat(partialCards).Select(CardViewModel.From));
+		}
+
+		private void _LocateAndTranslateFormulas(CardData card)
+		{
+			throw new System.NotImplementedException();
 		}
 	}
 }
