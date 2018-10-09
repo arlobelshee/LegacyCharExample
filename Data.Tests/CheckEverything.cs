3f5bd621263c14955304cddbe79f351d3a3a4b3d
Arlo Belshee <arlo.belshee@live.com>
2018-10-09 19:13:33 -0400

    t    Verify that the pipeline is the data flow that I expect.

---
+++ Data.Tests/CheckEverything.cs
@@ -0,0 +1,22 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Data.PipelineSynchronous;
+using NUnit.Framework;
+
+namespace Data.Tests
+{
+	[TestFixture]
+	[UseReporter(typeof(VisualStudioReporter))]
+
+	public class CheckEverything
+	{
+		[Test]
+		public void TestPipeline()
+		{
+
+			var pipeSource = DoEverything.MakePipe(out var characterCollector, out var configCollector);
+			Approvals.Verify( pipeSource);
+		}
+
+	}
+}
