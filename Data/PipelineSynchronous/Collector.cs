b12dbfe7cf3265b5f90c7a00ffa3bce5e91cda03
Jay Bazuzi <jay@bazuzi.com>
2019-02-17 14:08:20 -0800

    Add factory (Collector.For) and accessor (Collector.SingleResult)

--- Data/PipelineSynchronous/Collector.cs
+++ Data/PipelineSynchronous/Collector.cs
@@ -1,9 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq.
 
 namespace Data.PipelineSynchronous
 {
+	public static class Collector
+	{
+		public static Collector<T> For<_, T>(PipeThatCanBeFollowed<_, T> characterFileFrom)
+		{
+			var collector = new Collector<T>();
+			characterFileFrom.AndThen(collector);
+			return collector;
+		}
+		
+		public static T SingleResult<T>(this Collector<T> @this)
+		{
+		    return @this.Results.Single();
+		}
+	}
+
 	public class Collector<T> : IHandleResult<T>
 	{
 		private readonly List<T> _results = new List<T>();
